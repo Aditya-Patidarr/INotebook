6f37ebd20d61cab407951fe6ea2e4b932117ed61
aadi-30 <101740111+aadi-30@users.noreply.github.com>
2025-03-16 01:03:39 +0530

    Changes committed

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose'); 
  // 'mongodb://127.0.0.1:27017/iNotebook'
-const connectToMongo = async (dburl)=>{
+const connectToMongo = async(dburl)=>{
     await mongoose.connect(dburl)   
     .then(()=>{
         console.log("INotebook database is active!!!")
--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const connectToMongo = require('./db');
 const express = require('express');
 const dburl = process.env.DB_URL ;
 connectToMongo(dburl); 
-
+const PORT = 5000
 const app = express(); 
 
 app.use(express.json());
@@ -13,3 +13,9 @@ const cors = require('cors')
 app.use(cors())
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
+app.get('/',(req,res)=>{
+    res.send("Hello")
+})
+app.listen(PORT,(req,res)=>{
+    console.log(`Listening on Port ${PORT}`);
+})
